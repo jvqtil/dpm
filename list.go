75f8c15159e092c0af20c5cb70c6e73f354d892f
Egor <109822937+jvqtil@users.noreply.github.com>
2026-08-17 13:04:50 +0300

    move show  function to separate file

--- list.go
+++ list.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"os"
-	"slices"
-	"strings"
 	"text/tabwriter"
-	"time"
-
-	"github.com/dustin/go-humanize"
 )
 
 func listAll() error {
@@ -29,67 +23,5 @@ func listAll() error {
 	}
 	w.Flush()
 
-	return nil
-}
-
-func showPkgInfo(pkgName string) error {
-	reg, err := loadRegistry()
-	if err != nil {
-		return err
-	}
-	p, ok := reg.Packages[strings.ToLower(pkgName)]
-	if !ok {
-		return fmt.Errorf("package %s is not installed", pkgName)
-	}
-
-	fmt.Println(border)
-	fmt.Printf("%s - %s\n", green(p.Name), p.CurrentTag.Name)
-	fmt.Printf("%s\n", p.Source)
-	fmt.Printf("%s\n", p.BinaryPath)
-	fmt.Println()
-
-	tags := make([]string, len(p.Tags))
-	for i, t := range p.Tags {
-		tags[i] = t.Name
-	}
-	slices.Sort(tags)
-	slices.Reverse(tags)
-
-	for _, name := range tags {
-		if name == p.CurrentTag.Name {
-			fmt.Printf(" %s - current\n", name)
-		} else {
-			fmt.Printf(" %s\n", name)
-		}
-	}
-
-	fmt.Println()
-	if p.LastUpdated != p.InstalledAt {
-		t, _ := time.Parse(time.RFC3339, p.LastUpdated)
-		fmt.Printf("last updated: %s\n", humanize.Time(t))
-	}
-	t, _ := time.Parse(time.RFC3339, p.InstalledAt)
-	fmt.Printf("installed: %s\n", t.Format("02 Jan 06 15:04"))
-	fmt.Println(border)
-
-	return nil
-}
-
-func showPkgInfoJSON(pkgName string) error {
-	reg, err := loadRegistry()
-	if err != nil {
-		return err
-	}
-	pkg, ok := reg.Packages[strings.ToLower(pkgName)]
-	if !ok {
-		return fmt.Errorf("package %s is not installed", pkgName)
-	}
-
-	data, err := json.MarshalIndent(pkg, "", "  ")
-	if err != nil {
-		return err
-	}
-
-	fmt.Println(string(data))
 	return nil
 }
